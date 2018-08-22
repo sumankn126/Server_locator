6781da8693ac5d44af6ee7858ea12db6d1d68d0a
suman.nanjappa <suman.nanjappa@hotelsoft.com>
2018-08-22 16:12:44 +0530

    IP_finder.py upgraded

--- IP_finder.py
+++ IP_finder.py
@@ -1,12 +1,16 @@
 # finding the ip address
 import socket
-
+from urllib.parse import urlparse
 
 def ip_finder():
     url=input('\nEnter the url for IP tracing:')
-    print('\nRouting for IP address of : ',url)
-    print('\nIP Address is : ')
-    IP_Addr = socket.gethostbyname(url)
-    print(IP_Addr)
+    domname = urlparse(url)
+    if domname.netloc is '':
+        IP_Addr = socket.gethostbyname(domname.path)
+        print('\nRouting for IP address of : ',domname.path)
+    else:
+        IP_Addr = socket.gethostbyname(domname.netloc)
+        print('\nRouting for IP address of : ',domname.netloc)
+    print('\nIP Address is : ',IP_Addr)
     return IP_Addr
 
